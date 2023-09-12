281e7d76a566fe721341360c7d9045d2a7704bd0
silviadebussi <silviadebussi2@gmail.com>
2023-09-12 12:51:42 -0300

    'atualização'

--- sca/settings.py
+++ sca/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'sca.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sca',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
@@ -108,6 +112,8 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'America/Sao_Paulo'
+
+DEFAULT_CHARSET = 'utf-8'
 
 USE_I18N = True
 
